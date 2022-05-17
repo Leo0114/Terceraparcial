8a2911b87199340dbd5ea6937be1496644bc1b9b
Leo0114 <leonel0614@gmail.com>
2022-05-17 15:46:24 -0500

    ult

--- js/ninit.js
+++ js/ninit.js
@@ -1,30 +1,24 @@
-(function($){
-  $(function(){
-    $('.parallax').parallax();
-    $('.sidenav').sidenav();
-    $('.materialboxed').materialbox();
-    $('.collapsible').collapsible();
-    $('.tabs').tabs({swipeable:true});
-    $('.carousel').carousel();
-    $('.modal').modal();
-
-
-
-
-
+(function ($) {
+  $(function () {
+    $(".parallax").parallax();
+    $(".sidenav").sidenav();
+    $(".materialboxed").materialbox();
+    $(".collapsible").collapsible();
+    $(".tabs").tabs({ swipeable: true });
+    $(".carousel").carousel();
+    $(".modal").modal();
   });
-})(jQuery); 
-
- 
+})(jQuery);
 
 $(document).ready(function () {
-    $('.slider').slider({
-      indicators: true,
-  
-    
-    });
+  $(".slider").slider({
+    indicators: true,
   });
+  $(document).ready(function () {
+    $(".fixed-action-btn").floatingActionButton({ hoverEnabled: false });
+  });
+});
 
-  $(document).ready(function(){
-    $('.scrollspy').scrollSpy();
-  });
+$(document).ready(function () {
+  $(".scrollspy").scrollSpy();
+});
